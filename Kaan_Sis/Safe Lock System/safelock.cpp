0177c143dfccb071de2f7f4403c0d4338c538272
Kaan Sis <68927305+KaanSis@users.noreply.github.com>
2023-12-29 20:30:21 +0300

    Update safelock.cpp

--- Kaan_Sis/Safe Lock System/safelock.cpp
+++ Kaan_Sis/Safe Lock System/safelock.cpp
@@ -8,9 +8,9 @@ LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
 
 #define Password_Lenght 5 // Give enough room for six chars + NULL char
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;    
 
-char Data[Password_Lenght]; // 6 is the number of chars it can hold + the null char = 7
+char Data[Password_Lenght]; 
 char Master[Password_Lenght] = "1234";
 byte data_count = 0, master_count = 0;
 bool Pass_is_good;
@@ -26,10 +26,10 @@ char keys[ROWS][COLS] = {
 };
 bool door = true;
 
-byte rowPins[ROWS] = {1, 2, 3, 4}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {5, 6, 7, 8}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {1, 2, 3, 4}; 
+byte colPins[COLS] = {5, 6, 7, 8}; 
 
-Keypad customKeypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS); //initialize an instance of class NewKeypad
+Keypad customKeypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS); 
 
 void setup()
 {
@@ -69,26 +69,26 @@ void loop()
 void clearData()
 {
   while (data_count != 0)
-  { // This can be used for any array size,
-    Data[data_count--] = 0; //clear array for new data
+  {
+    Data[data_count--] = 0; 
   }
   return;
 }
 
 void ServoOpen()
 {
-  for (pos = 180; pos >= 0; pos -= 5) { // goes from 0 degrees to 180 degrees
+  for (pos = 180; pos >= 0; pos -= 5) { 
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    myservo.write(pos);              
+    delay(15);                       
   }
 }
 
 void ServoClose()
 {
-  for (pos = 0; pos <= 180; pos += 5) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 0; pos <= 180; pos += 5) { 
+    myservo.write(pos);             
+    delay(15);                       
   }
 }
 
@@ -98,17 +98,17 @@ void Open()
   lcd.print(" Enter Password");
   
   customKey = customKeypad.getKey();
-  if (customKey) // makes sure a key is actually pressed, equal to (customKey != NO_KEY)
+  if (customKey) 
   {
-    Data[data_count] = customKey; // store char into data array
-    lcd.setCursor(data_count, 1); // move cursor to show each new char
-    lcd.print(Data[data_count]); // print char at said cursor
-    data_count++; // increment data array by 1 to store new char, also keep track of the number of chars entered
+    Data[data_count] = customKey; 
+    lcd.setCursor(data_count, 1); 
+    lcd.print(Data[data_count]); 
+    data_count++; 
   }
 
-  if (data_count == Password_Lenght - 1) // if the array index is equal to the number of expected chars, compare data to master
+  if (data_count == Password_Lenght - 1) 
   {
-    if (!strcmp(Data, Master)) // equal to (strcmp(Data, Master) == 0)
+    if (!strcmp(Data, Master)) 
     {
       lcd.clear();
       digitalWrite(10,HIGH);
